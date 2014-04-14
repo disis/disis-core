3aa6fc09aa858d1c36ea61eb0d608bdb037c65d4
Jirka Penzes <jirkapenzes@gmail.com>
2014-04-14 20:52:12 +0200

    Initial commit - #first disis core prototype

---
+++ src/disis/core/DisisCommunicator.java
@@ -0,0 +1,20 @@
+package disis.core;
+
+/**
+ * This is DISIS
+ * Authors: Jirka Penzes & Jan Voracek
+ * Date: 14/04/14 20:27
+ */
+public class DisisCommunicator {
+    public void start(LocalConfiguration localConfiguration) {
+
+    }
+
+    public boolean connect(DisisConfiguration disisConfiguration) {
+        return true;
+    }
+
+    public void distributeBroadcastInternalMessage() {
+
+    }
+}
---
+++ src/disis/core/DisisConfiguration.java
@@ -0,0 +1,9 @@
+package disis.core;
+
+/**
+ * This is DISIS
+ * Authors: Jirka Penzes & Jan Voracek
+ * Date: 14/04/14 20:20
+ */
+public class DisisConfiguration {
+}
