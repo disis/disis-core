e4709e894cb143f7bf219e069e42666015a7969e
Jan Voráček <jan@voracek.net>
2014-04-27 16:00:09 +0200

    Fixed dealing with local/remote RMI registries

--- src/disis/core/DisisCommunicator.java
+++ src/disis/core/DisisCommunicator.java
@@ -5,10 +5,11 @@ import disis.core.configuration.LocalConfiguration;
 import disis.core.rmi.IMessageInbox;
 import disis.core.rmi.RmiMessageInbox;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
+import java.rmi.ConnectException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,25 +29,48 @@ public class DisisCommunicator {
             localMessageInbox.onReceivedMessage(message -> {
                 // do something
             });
-            Naming.rebind(localConfiguration.getLocalName(), localMessageInbox);
-        } catch (RemoteException | MalformedURLException e) {
+            getLocalRmiRegistry(localConfiguration.getLocalPort()).rebind(localConfiguration.getLocalName(), localMessageInbox);
+        } catch (RemoteException e) {
             e.printStackTrace();
         }
 
     }
 
-    public boolean connect(DisisConfiguration disisConfiguration) {
+    private Registry getLocalRmiRegistry(int port) throws RemoteException {
+
+        Registry registry = LocateRegistry.getRegistry(port);
+        if (isRegistryRunning(registry))
+            return registry;
+        else
+            return LocateRegistry.createRegistry(port);
+    }
+
+    private boolean isRegistryRunning(Registry registry) throws RemoteException {
         try {
-            IMessageInbox remoteMessageInbox = (IMessageInbox) Naming.lookup(disisConfiguration.getRemoteName());
-            remoteMessageInboxes.put(disisConfiguration.getRemoteName(), remoteMessageInbox);
+            registry.list();
             return true;
-        } catch (NotBoundException | MalformedURLException | RemoteException e) {
+        } catch (ConnectException e){
             return false;
         }
     }
 
+    public boolean connect(DisisConfiguration disisConfiguration) {
+        try {
+            Registry registry = getRemoteRmiRegistry(disisConfiguration.getNetworkAddress(), disisConfiguration.getPort());
+            IMessageInbox remoteMessageInbox = (IMessageInbox) registry.lookup(disisConfiguration.getRemoteName());
+            remoteMessageInboxes.put(disisConfiguration.getRemoteName(), remoteMessageInbox);
+            return true;
+        } catch (NotBoundException | RemoteException e) {
+            return false;
+        }
+    }
+
+    private Registry getRemoteRmiRegistry(String networkAddress, int port) throws RemoteException {
+        return LocateRegistry.getRegistry(networkAddress, port);
+    }
+
     public void sendInternalBroadcastMessage(InternalMessage message) {
-        for(IMessageInbox messageInbox : remoteMessageInboxes.values()) {
+        for (IMessageInbox messageInbox : remoteMessageInboxes.values()) {
             try {
                 messageInbox.sendMessage(message);
             } catch (RemoteException e) {
